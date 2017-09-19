1d348c2339a4f7a8bc51874855d14cdca5c89bf2
Jeff Whelpley <jeff@gethuman.com>
2017-09-19 09:34:15 -0400

    Scaffolding working on server

--- src/app/app-browser.module.ts
+++ src/app/app-browser.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CurrentPlatform } from './platform';
+import { CreateComponent } from './create/create.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule.withServerTransition({ appId: 'universal' })],
+    declarations: [AppComponent, CreateComponent, HomeComponent, LoginComponent, SearchComponent, TicketComponent],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'universal' }),
+        RouterModule.forRoot([
+            { path: '', component: HomeComponent },
+            { path: 'x/search', component: SearchComponent },
+            { path: 'x/create', component: CreateComponent },
+            { path: 'x/login', component: LoginComponent },
+            { path: 'ticket/:ticketId', component: TicketComponent }
+        ])
+    ],
     providers: [{ provide: CurrentPlatform, useValue: 'Browser' }],
     bootstrap: [AppComponent]
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { CurrentPlatform } from './platform';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    template: `
+        <a routerLink="/">Home</a> | 
+        <a routerLink="/x/search">Search Tickets</a> | 
+        <a routerLink="/x/create">Create Ticket</a> | 
+        <a routerLink="/x/login">Login</a>
+        <router-outlet></router-outlet>
+    `
 })
 export class AppComponent {
     title = 'app';
